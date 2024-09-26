5021f02bd0615938d5acb8caaeacb4ebe985f063
Arafian01 <arafianw@gmail.com>
2024-09-26 07:40:09 +0700

    V1 Konsumen

--- kasir/app/Http/Controllers/KonsumenController.php
+++ kasir/app/Http/Controllers/KonsumenController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Konsumen;
 use Illuminate\Http\Request;
 
 class KonsumenController extends Controller
@@ -11,7 +12,10 @@ class KonsumenController extends Controller
      */
     public function index()
     {
-        return view ('page.konsumen.index');
+        $konsumen = Konsumen::all();
+        return view('page.konsumen.index')->with([
+            'konsumen' => $konsumen
+        ]);
     }
 
     /**
@@ -27,7 +31,14 @@ class KonsumenController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = [
+            'konsumen' => $request->input('konsumen'),
+            'status' => $request->input('status'),
+        ];
+
+        Konsumen::create($data);
+
+        return back()->with('message_delete', 'Data Konsumen Sudah dihapus');
     }
 
     /**
@@ -51,7 +62,14 @@ class KonsumenController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $data = [
+            'konsumen' => $request->input('konsumen'),
+            'status' => $request->input('status'),
+        ];
+
+        $datas = Konsumen::findOrFail($id);
+        $datas->update($data);
+        return back()->with('message_delete', 'Data Konsumen Sudah dihapus');
     }
 
     /**
@@ -59,6 +77,8 @@ class KonsumenController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $data = Konsumen::findOrFail($id);
+        $data->delete();
+        return back()->with('message_delete','Data Konsumen Sudah dihapus');
     }
 }
