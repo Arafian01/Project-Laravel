d2e6efce282f79e2444437fbd525de8484d770ba
Arafian01 <arafianw@gmail.com>
2024-10-04 21:16:26 +0700

    V6 Konsinyasi

--- kasir/resources/views/layouts/navigation.blade.php
+++ kasir/resources/views/layouts/navigation.blade.php
@@ -47,7 +47,7 @@
                                     {{ __('Supplier') }}
                                 </x-dropdown-link>
     
-                                <x-dropdown-link :href="route('dashboard')">
+                                <x-dropdown-link :href="route('konsinyasi.index')">
                                     {{ __('Konsinyasi') }}
                                 </x-dropdown-link>
     
