cd9c3feb8e1f5251abc62f6c6355e9152602f8d8
Arafian01 <arafianw@gmail.com>
2024-11-05 13:26:07 +0700

    WEB PROG V1

---
+++ kasir/resources/views/page/penjualan/index.blade.php
@@ -0,0 +1,63 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Penjualan') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <div class="relative overflow-x-auto">
+                        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <thead
+                                class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" class="px-6 py-3">
+                                        NO
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        KONSUMEN
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        TANGGAL
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        STATUS PEMBELIAN
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        ACTION
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($penjualan as $p)
+                                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                        <th scope="row"
+                                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            Apple MacBook Pro 17"
+                                        </th>
+                                        <td class="px-6 py-4">
+                                            Silver
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            Laptop
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            $2999
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            $2999
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- kasir/routes/web.php
+++ kasir/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Konsinyasi_ProdukController;
 use App\Http\Controllers\KonsinyasiController;
 use App\Http\Controllers\KonsumenController;
+use App\Http\Controllers\PenjualanController;
 use App\Http\Controllers\ProdukController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SupplierController;
@@ -17,6 +18,7 @@ Route::resource('supplier', SupplierController::class)->middleware('auth');
 Route::resource('produk', ProdukController::class)->middleware('auth');
 Route::resource('konsinyasi', KonsinyasiController::class)->middleware('auth');
 Route::resource('konsinyasi_produk', Konsinyasi_ProdukController::class)->middleware('auth');
+Route::resource('penjualan', PenjualanController::class)->middleware('auth');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
